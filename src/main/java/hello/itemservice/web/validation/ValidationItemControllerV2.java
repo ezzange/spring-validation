2dc4d2d45a0d97cc59f329427fafd79f1dbbb89d
ezzange <ezzang22@naver.com>
2023-11-14 04:09:55 +0900

    spring-ValidationV3 errors_en.properties 파일을 생성해서 오류 메세지를 국제화 처리

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -84,7 +84,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV2(//BindingResult bindingResult 파라미터의 위치는 @ModelAttribute Item item 다음에 와야 한다.
                             @ModelAttribute Item item,
                             BindingResult bindingResult,
@@ -114,6 +114,50 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", null, null,
                         "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 : " + resultPrice));
+            }
+        }
+        //검증에 실패하면 다시 입력 폼으로
+        if (bindingResult.hasErrors()) {
+            log.info("errors = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+        //성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+    @PostMapping("/add")
+    public String addItemV3(//BindingResult bindingResult 파라미터의 위치는 @ModelAttribute Item item 다음에 와야 한다.
+                            @ModelAttribute Item item,
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes,
+                            Model model
+    ) {
+
+        //검증 로직
+        if (!StringUtils.hasText(item.getItemName())) {
+            log.info("itemName", item.getItemName());
+            bindingResult.addError(new FieldError("item", "itemName",
+                    item.getItemName(), false, new String[]{"required.item.itemName","required.default"}, null, null));
+        }                                                                     //배열로 넣게되면 만약 errors.peoperties에서 첫번째 오류 메세지를 못 찾을 경우 두번째 오류 메세지가 자동으로 들어간다.
+                                                                              //만약 두번째도 찾지 못한다면 defaultMessage
+                                                                              //defaultMessage 도 없으면 500 Error
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.addError(new FieldError("item", "price",
+                    item.getPrice(),false,new String[]{"range.item.price"},new Object[]{1000, 1000000},null));
+
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item", "quantity",
+                    item.getQuantity(),false, new String[]{"max.item.quantity"}, new Object[]{9999}, null));
+
+        }
+        //특정 필드가 아닌 복합 룰 검증
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
             }
         }
         //검증에 실패하면 다시 입력 폼으로
