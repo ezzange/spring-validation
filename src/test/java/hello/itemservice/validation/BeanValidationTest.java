1df92060fdfc206b72a491d2cc444b8f3cc55506
ezzange <ezzang22@naver.com>
2023-11-14 13:39:44 +0900

    spring - Bean Validation

---
+++ src/test/java/hello/itemservice/validation/BeanValidationTest.java
@@ -0,0 +1,33 @@
+package hello.itemservice.validation;
+
+import hello.itemservice.domain.item.Item;
+import org.junit.jupiter.api.Test;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.Set;
+
+public class BeanValidationTest {
+
+    @Test
+    void beanValidation() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        Validator validator = factory.getValidator();
+        //검증기 생성
+        //다음 코드와 같이 검증기를 생성한다.
+        //이후 스프링과 통합하면 우리가 직접 이런 코드를 작성하지는 않는다.
+
+        Item item = new Item();
+        item.setItemName(" ");
+        item.setPrice(0);
+        item.setQuantity(10000);
+
+        Set<ConstraintViolation<Item>> validations = validator.validate(item);
+        for (ConstraintViolation<Item> validation : validations) {
+            System.out.println("validation = " + validation);
+            System.out.println("validations = " + validation.getMessage());
+        }
+    }
+}
