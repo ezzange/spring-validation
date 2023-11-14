7823ea407ffe2f6a9ef4e8a76d4a16c725d7ae78
ezzange <ezzang22@naver.com>
2023-11-15 06:25:45 +0900

    spring - Bean Validation - Object 오류

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV3.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -50,6 +51,14 @@ public class ValidationItemControllerV3 {
             RedirectAttributes redirectAttributes
     ) {
 
+         //특정 필드가 아닌 복합 룰 검증 -   item 클래스에 @ScriptAssert을 사용해서 분리할 수 있지만 번거로워 컨트롤러 코드로 가져가는 것이 낫다
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.addError(new ObjectError("item", null, null,
+                        "가격 * 수량의 합은 10,000원 이상이여야 합니다. 현재 값 : " + resultPrice));
+            }
+        }
         //검증에 실패하면 다시 입력 폼으로
         if (bindingResult.hasErrors()) {
             log.info("errors = {}", bindingResult);
