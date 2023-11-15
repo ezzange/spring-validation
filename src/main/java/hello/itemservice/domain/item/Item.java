96afaf035a52009fdb0e282a1d82ebe89cd0cace
ezzange <ezzang22@naver.com>
2023-11-16 06:15:07 +0900

    spring - Bean Validation Form전송 객체 분리

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -10,15 +10,15 @@ import javax.validation.constraints.NotNull;
 @Data
 //@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격과 수량이 조절하세요")
 public class Item {
-
+//    @NotNull(groups = UpdateCheck.class)
     private Long id;
-    @NotBlank(groups = UpdateCheck.class)
+//    @NotBlank(groups = UpdateCheck.class)
     private String itemName;
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,UpdateCheck.class})
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {SaveCheck.class,UpdateCheck.class})
     private Integer price;
-    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
-    @Max(value = 9999,groups = SaveCheck.class)
+//    @NotNull(groups = {SaveCheck.class,UpdateCheck.class})
+//    @Max(value = 9999,groups = SaveCheck.class)
     private Integer quantity;
 
     public Item() {
